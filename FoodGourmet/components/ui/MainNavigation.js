20d4475f3bba4e44fb4780b6ce37ee15a49f44a0
Jerry Jiang <jerryjiang49@gmail.com>
2023-03-17 23:15:19 -0400

    updating font, background color, and more

--- FoodGourmet/components/ui/MainNavigation.js
+++ FoodGourmet/components/ui/MainNavigation.js
@@ -1,8 +1,16 @@
 import styles from './MainNavigation.module.scss';
+import Link from "next/link";
+
 export default function MainNavigation() {
     return (
-        <div className={styles.header}>
-
-        </div>
+        <nav className={styles.header}>
+            <Link href={'/'} className={styles.header__link}>
+                <h1>Epicurean</h1>
+            </Link>
+            <Link href={'/recipes'} className={styles.header__link}>
+                Leaderboard
+            </Link>
+            <button type="button" className={styles.header__link}>Login</button>
+        </nav>
     )
 }
