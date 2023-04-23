36ba0fb6ab7cafdf8a4b1f2101e74c90fd86e4b0
Jerry Jiang <jerryjiang49@gmail.com>
2023-04-22 22:26:32 -0400

    updated axios backend url

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'epicurean-backend-12ibezt1k-jjiang0595.vercel.app'
 })
 
 export default api;
