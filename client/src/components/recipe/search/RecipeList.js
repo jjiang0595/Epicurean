2559a88e4a4f96f5a45f2bbec80bab66e0222b23
Jerry Jiang <jerryjiang49@gmail.com>
2023-04-22 20:16:48 -0400

    fixed recipes list displaying incorrectly

--- client/src/components/recipe/search/RecipeList.js
+++ client/src/components/recipe/search/RecipeList.js
@@ -4,15 +4,16 @@ import RecipeItem from "./RecipeItem";
 function RecipeList(props) {
     return (
         <div className={styles.list}>
-            {props.recipes.length > 0 ? props.recipes.map((recipe) => (
             <div className={styles.list__recipes}>
+                {props.recipes.length > 0 ? props.recipes.map((recipe) => (
                     <RecipeItem key={recipe.key}
                                 recipeId={recipe.key}
                                 title={recipe.title}
                                 image={recipe.image}
                                 description={recipe.instr}
                     />
-            </div>)) : <h1 className={styles.list__noRecipes}>Sorry, there are no results for your search term.</h1>}
+                )) : <h1 className={styles.list__noRecipes}>Sorry, there are no results for your search term.</h1>}
+            </div>
         </div>
     );
 }
